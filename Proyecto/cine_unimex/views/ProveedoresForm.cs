9a27da858dce85eceda254a101455bb1dd06a0e8
MarioSuarezC <marioduke1994@hotmail.com>
2015-03-29 20:01:07 -0600

    Proveedores DTO, DAO, y FORM
    subo mi catalogo de proveedores

---
+++ Proyecto/cine_unimex/views/ProveedoresForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using unimex.lenguajesv.cine.DAO;
+
+// Catalogo realizado por Gonzalez Reyes Berenice Izamar
+
+namespace unimex.lenguajesv.cine.views
+{
+    public partial class ProveedoresForm : Form
+    {
+        public ProveedoresForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ProveedoresForm_Load(object sender, EventArgs e)
+        {
+            ConsultarProveedor();
+        }
+        private void ConsultarProveedor()
+        {
+            ProveedoresDAO Z_DAO = new ProveedoresDAO();
+
+            try
+            {
+                DataTable usr = Z_DAO.LoadProveedor();
+                viewProveedor.DataSource = usr;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex);
+            }
+        }
+    }
+}
