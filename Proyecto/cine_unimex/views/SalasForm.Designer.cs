38e3c563d01b900720b18c6344d60e72c31ad28a
jackblackte <darrellzx@hotmail.com>
2015-04-03 16:13:25 -0600

    Salas

---
+++ Proyecto/cine_unimex/views/SalasForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace unimex.lenguajesv.cine.views
+{
+    partial class SalasForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataG = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataG
+            // 
+            this.dataG.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataG.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataG.Location = new System.Drawing.Point(208, 33);
+            this.dataG.Name = "dataG";
+            this.dataG.RowTemplate.Height = 24;
+            this.dataG.Size = new System.Drawing.Size(733, 576);
+            this.dataG.TabIndex = 0;
+            // 
+            // ComplejosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 629);
+            this.Controls.Add(this.dataG);
+            this.Name = "ComplejosForm";
+            this.Text = "ComplejosForm";
+            this.Load += new System.EventHandler(this.ComplejosForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataG)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataG;
+
+    }
+}
---
+++ Proyecto/cine_unimex/views/SalasForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using unimex.lenguajesv.cine.DAO;
+
+namespace unimex.lenguajesv.cine.views
+{
+    public partial class SalasForm : Form
+    {
+        public SalasForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ComplejosForm_Load(object sender, EventArgs e)
+        {
+            consultaComplejos();
+
+        }
+        private void consultaComplejos()
+        {
+            SalasDAO Com_DAO = new SalasDAO();
+            try
+            {
+                DataTable dt = Com_DAO.loadSalas();
+                dataG.DataSource = dt;
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("" + ex);
+ 
+            }
+        }
+    }
+}
