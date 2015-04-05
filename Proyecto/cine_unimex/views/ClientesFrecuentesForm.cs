9f0531d17b8f2f09fe15a24c181b43215181ebdc
JorgeQuintana <jorge.qs@live.com.mx>
2015-04-05 14:55:06 -0500

    DAO, DTO Y FORMS ClientesFrecuentes

---
+++ Proyecto/cine_unimex/views/ClientesFrecuentesForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace unimex.lenguajesv.cine.views
+{
+    public partial class ClientesFrecuentesForm : Form
+    {
+        public ClientesFrecuentesForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
