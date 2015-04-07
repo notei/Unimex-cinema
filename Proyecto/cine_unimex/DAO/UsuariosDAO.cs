093c843bb014698fd000377d3570920841bb04b7
SandraMiranda <smiranda3191@hotmail.com>
2015-04-06 19:53:49 -0500

    UPdate
    catalogo usuarios

--- Proyecto/cine_unimex/DAO/UsuariosDAO.cs
+++ Proyecto/cine_unimex/DAO/UsuariosDAO.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 using unimex.lenguajesv.cine.DTO;
 
 namespace unimex.lenguajesv.cine.DAO
 {
     class UsuariosDAO
     {
-        /*Corrijo consulta, agregando "FROM" debido a que solo pedia campos pero no daba la ubicación de la tabla
-        Y en el mainScreen agrego al tool strip usuarios y mando llamar al formulario */
+
         private String SQL_Consulta_Usuarios = "select usuarios.idUsuario AS Id , usuarios.nombre AS Nombre, usuarios.apellido_paterno AS ApPaterno, usuarios.nombre_usuario AS NombreUsuario, usuarios.contrasena AS Contraseña, usuarios.id_tipo_usuario AS TipoUsuario from usuarios";
         private String Cadena = Properties.Resources.CADENA_CONEXION;
 
@@ -32,10 +31,7 @@ namespace unimex.lenguajesv.cine.DAO
 
         public void agregarUsuario(UsuariosDTO n)
         {
-            /* verificar consulta con respecto al nombre de usuario, no necesita ser ingresado
-             * Faltan apostrofos (comillas simples) para los campos de tipo texto
-             * Modifico campo nombre para mostrar ejemplo.
-             */
+
             String SQL_Agregar_Usuario = " insert into usuarios (nombre, apellido_paterno, nombre_usuario, contrasena, id_tipo_usuario) values ('" + n.Nombre + "' , '" + n.ApPaterno + "','" + n.NomUsuario + "','" + n.Contrasena + "','" + n.Id_Tipo_Usuario + "')";
 
             SqlConnection con = new SqlConnection();
@@ -45,9 +41,87 @@ namespace unimex.lenguajesv.cine.DAO
             SqlCommand cmd = new SqlCommand(SQL_Agregar_Usuario, con);
             cmd.ExecuteNonQuery();
             con.Close();
-
-
         }
 
+        public UsuariosDTO cargarUsuariosUpdate(int id)
+        {
+            String SQL_Cargar_Usuario = "select * from usuarios where idUsuario = " + id;
+            UsuariosDTO useredit = new UsuariosDTO();
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = Cadena;
+            con.Open();
+            SqlCommand cmd = new SqlCommand(SQL_Cargar_Usuario, con);
+            SqlDataReader red = cmd.ExecuteReader();
+            while (red.Read())
+            {
+                useredit.idusuario = (int)red.GetSqlInt32(0);
+                useredit.Nombre = (String)red.GetSqlString(1);
+                useredit.ApPaterno = (String)red.GetSqlString(2);
+                useredit.NomUsuario = (String)red.GetSqlString(3);
+                useredit.Contrasena = (String)red.GetSqlString(4);
+                useredit.Id_Tipo_Usuario = (int)red.GetSqlInt32(5);
+            }
+            con.Close();
+            return useredit;
+        }
+
+        public void updateUsuariosDTO(UsuariosDTO preciodtoup)
+        {
+            String SQL_Update_Usuario = " UPDATE usuarios SET nombre = '" + preciodtoup.Nombre + "', apellido_paterno = '" + preciodtoup.ApPaterno + "', nombre_usuario = " + preciodtoup.NomUsuario + "', contrasena = " + preciodtoup.Contrasena + "', id_tipo_usuario = " + preciodtoup.Id_Tipo_Usuario + " WHERE idUsuario = " + preciodtoup.idusuario;
+            SqlConnection con;
+            con = new SqlConnection();
+            con.ConnectionString = Cadena;
+            con.Open();
+            SqlCommand comando;
+            comando = new SqlCommand(SQL_Update_Usuario, con);
+            comando.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void deleteUsuarioDTO(UsuariosDTO deletedto)
+        {
+            String SQL_Update_Usuario = " DELETE FROM usuarios WHERE idUsuario = " + deletedto.idusuario;
+            SqlConnection con;
+            con = new SqlConnection();
+            con.ConnectionString = Cadena;
+            con.Open();
+            SqlCommand comando;
+            comando = new SqlCommand(SQL_Update_Usuario, con);
+            comando.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public DataTable cargaBusquedaUsuario(UsuariosDTO busdtopre)
+        {
+            String SQL_Buscar_Usuario = "select usuarios.idUsuario AS Id , usuarios.nombre AS Nombre, usuarios.apellido_paterno AS Apellido, usuarios.nombre_usuario AS Usario, usuarios.contrasena AS Password, usuarios.id_tipo_usuario AS TUsuario  from usuarios where usuarios.idUsuario = " + busdtopre.idusuario;
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = Cadena;
+            con.Open();
+            using (SqlDataAdapter adapter1 = new SqlDataAdapter(SQL_Buscar_Usuario, con))
+            {
+
+                DataTable tblpbus = new DataTable();
+                adapter1.Fill(tblpbus);
+                con.Close();
+                return tblpbus;
+            }
+        }
+
+
+        public DataTable LoadNombreUsuario()
+        {
+            String SQL_Busc_Usuario = "select idUsuario, nombre from usuarios";
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = Cadena;
+            con.Open();
+            using (SqlDataAdapter adapter = new SqlDataAdapter(SQL_Busc_Usuario, con))
+            {
+
+                DataTable tbl = new DataTable();
+                adapter.Fill(tbl);
+                con.Close();
+                return tbl;
+            }
+        }
     }
 }
