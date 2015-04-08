7eecf813ad6d4c60865385308fcf9b1554b73c83
Alejandro G. Leguízamo <cbsoul1987@gmail.com>
2015-04-07 22:09:07 -0500

    DTO Punto de venta

---
+++ Proyecto/cine_unimex/DTO/VentaProyeccionesDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace unimex.lenguajesv.cine.DTO
+{
+    class VentaProyeccionesDTO
+    {
+        private int id_venta_proyecciones;
+        private int id_proyeccion;
+        private int cantidad_boletos;
+        private int id_precio;
+        private int id_cliente_frecuente;
+        private int num_transaccion;
+        private float precio_unitario;
+        private float precio_total;
+        private int id_registro_venta;
+
+
+
+    }
+}
