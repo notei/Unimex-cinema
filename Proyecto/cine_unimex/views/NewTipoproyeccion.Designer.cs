de96208be79f8a58356026f368d522a07909c790
loza25 <lloza_@outlook.com>
2015-04-07 22:33:50 -0500

    "Diseño"

--- Proyecto/cine_unimex/views/NewTipoproyeccion.Designer.cs
+++ Proyecto/cine_unimex/views/NewTipoproyeccion.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewTipoproyeccion));
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -36,39 +37,38 @@
             this.txtDescripcion = new System.Windows.Forms.TextBox();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(38, 43);
+            this.label4.Location = new System.Drawing.Point(24, 31);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(96, 16);
             this.label4.TabIndex = 0;
-            this.label4.Text = "label1";
+            this.label4.Text = "Tipo Proyeccion:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(38, 83);
+            this.label5.Location = new System.Drawing.Point(24, 82);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(73, 16);
             this.label5.TabIndex = 1;
-            this.label5.Text = "label1";
+            this.label5.Text = "Descripción:";
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(38, 147);
+            this.label6.Location = new System.Drawing.Point(0, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "label1";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 8;
             // 
             // checkHabilitado
             // 
             this.checkHabilitado.AutoSize = true;
-            this.checkHabilitado.Location = new System.Drawing.Point(79, 147);
+            this.checkHabilitado.Location = new System.Drawing.Point(140, 149);
             this.checkHabilitado.Name = "checkHabilitado";
             this.checkHabilitado.Size = new System.Drawing.Size(15, 14);
             this.checkHabilitado.TabIndex = 3;
@@ -76,14 +76,14 @@
             // 
             // txtTipoproyeccion
             // 
-            this.txtTipoproyeccion.Location = new System.Drawing.Point(79, 40);
+            this.txtTipoproyeccion.Location = new System.Drawing.Point(140, 31);
             this.txtTipoproyeccion.Name = "txtTipoproyeccion";
-            this.txtTipoproyeccion.Size = new System.Drawing.Size(100, 20);
+            this.txtTipoproyeccion.Size = new System.Drawing.Size(100, 21);
             this.txtTipoproyeccion.TabIndex = 4;
             // 
             // txtDescripcion
             // 
-            this.txtDescripcion.Location = new System.Drawing.Point(79, 80);
+            this.txtDescripcion.Location = new System.Drawing.Point(140, 82);
             this.txtDescripcion.Multiline = true;
             this.txtDescripcion.Name = "txtDescripcion";
             this.txtDescripcion.Size = new System.Drawing.Size(100, 45);
@@ -91,9 +91,9 @@
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(27, 212);
+            this.btnAgregar.Location = new System.Drawing.Point(42, 190);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.Size = new System.Drawing.Size(68, 45);
             this.btnAgregar.TabIndex = 6;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
@@ -101,17 +101,27 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(140, 212);
+            this.button4.Location = new System.Drawing.Point(160, 190);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(69, 45);
             this.button4.TabIndex = 7;
             this.button4.Text = "Cancelar";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click_1);
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Habilitado:";
+            // 
             // NewTipoproyeccion
             // 
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(266, 267);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.txtDescripcion);
@@ -120,6 +130,8 @@
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "NewTipoproyeccion";
             this.Load += new System.EventHandler(this.NewTipoProyeccion_Load);
             this.ResumeLayout(false);
@@ -145,5 +157,6 @@
         private System.Windows.Forms.TextBox txtDescripcion;
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
     }
 }
